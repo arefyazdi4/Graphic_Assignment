eb9c321247f0d1f76f510ba6b7c67ca61380a74a
RF_BURGER <arefyazdi4@gmail.com>
2022-06-14 23:36:11 +0430

    Updating Noise canceling Comments

--- project_9_noise.py
+++ project_9_noise.py
@@ -8,29 +8,32 @@ from skimage.util import random_noise
 
 if __name__ == '__main__':
     # Load the image and show some basic information on it
-    image: np.ndarray = cv2.imread(r'A:/University/Term4/Graphic/Assignment/trex.png')
+    image: np.ndarray = cv2.imread(r'A:/University/Term4/Graphic/Assignment/coins.png')
 
-    image_gray: np.ndarray = cv2.imread(r'A:/University/Term4/Graphic/Assignment/trex.png', 0)  # gray read
+    image_gray: np.ndarray = cv2.imread(r'A:/University/Term4/Graphic/Assignment/coins.png', 0)  # gray read
     cv2.imshow("Image_gray", image_gray)
 
+    # it's like s&p but increase or decrease random value and random pixels by variance of 0
     image_noise_gaussian: np.ndarray = random_noise(image=image_gray, mode='gaussian')  # result num is array range 0 to 1.
-    image_noise: np.ndarray = random_noise(image=image_gray, mode='s&p')  # result num is array range 0 to 1.
+    image_noise_gaussian: np.ndarray = np.array(image_noise_gaussian * 255, dtype='uint8')
     cv2.imshow("Image_gaussian", image_noise_gaussian)
-    # it's like s&p but increase or decrease random value and random pixels
 
-    image_noise: np.ndarray = np.array(image_noise*255, dtype='uint8')
-    # it increase or decrease one pixel with avg 0_1
-    #
-    cv2.imshow("Image_noise", image_noise)
+    # salt and paper filter randomly put turns some pixels to 0 or 256
+    image_noise_sp: np.ndarray = random_noise(image=image_gray, mode='s&p')  # result num is array range 0 to 1.
+    image_noise_sp: np.ndarray = np.array(image_noise_sp * 255, dtype='uint8')
+    cv2.imshow("Image_sp", image_noise_sp)
 
-    image_filter = cv2.fastNlMeansDenoising(image_noise, None, 20, 20, 25)  # src, dsd = distnation, h= filter power, serach window, blocksize = number pixel
     # this is best for gaussian
     # 1/9 value filter applies on each pixel
-    cv2.imshow("Image_filter", image_filter)
-    image_filter = cv2.medianBlur(image_noise, 7, None)
+    # src, dsd = destination, h= filter power, search window, block size = number pixel
+    image_de_noise = cv2.fastNlMeansDenoising(image_noise_gaussian, None, 20, 20, 25)
+    cv2.imshow("Image_gau_deNoise", image_de_noise)
+
     # this is best for salt and paper
     # it will sort adj pixels around (expect it self) and select mid on
     # conclusion
+    image_de_noise = cv2.medianBlur(image_noise_sp, 7, None)
+    cv2.imshow("Image_s&p_deNoise", image_de_noise)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
