6d70ca915ef8d177134db4195fa13a7fd7e4a45a
RF_BURGER <arefyazdi4@gmail.com>
2022-06-22 13:47:03 +0430

    Creating Face Detection

---
+++ project_14_face_detection/detect_faces.py
@@ -0,0 +1,28 @@
+# USAGE
+# python detect_faces.py --face cascades/haarcascade_frontalface_default.xml --image images/obama.png
+
+# import the necessary packages
+from __future__ import print_function
+from pyimagesearch.facedetector import FaceDetector
+import argparse
+import cv2
+
+if __name__ == '__main__':
+
+    # load the image and convert it to grayscale
+    image = cv2.imread(r'images/obama2.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # find faces in the image
+    fd = FaceDetector(r'cascades/haarcascade_frontalface_default.xml')
+    faceRects = fd.detect(gray, scaleFactor=1.1, minNeighbors=5,
+                          minSize=(30, 30))
+    print("I found {} face(s)".format(len(faceRects)))
+
+    # loop over the faces and draw a rectangle around each
+    for (x, y, w, h) in faceRects:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # show the detected faces
+    cv2.imshow("Faces", image)
+    cv2.waitKey(0)
